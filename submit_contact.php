e120e094c35862b2b540e24aed9b833b434729d1
Neptune2k21 <116343729+Neptune2k21@users.noreply.github.com>
2024-09-10 14:14:20 +0200

    Contrôlez la valeur des paramètres

--- submit_contact.php
+++ submit_contact.php
@@ -24,6 +24,16 @@
             return;
         }
         ?>
+        <?php
+            if (
+            (!isset($_GET['email']) || !filter_var($_GET['email'], FILTER_VALIDATE_EMAIL))
+            || (!isset($_GET['message']) || empty($_GET['message']))
+            )
+                {
+            echo('Il faut un email et un message valides pour soumettre le formulaire.');
+            return;
+            }
+        ?>
 
         <h1>Message bien reçu !</h1>
         <div class="card">
